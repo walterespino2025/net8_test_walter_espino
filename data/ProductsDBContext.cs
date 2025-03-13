192127d45c1ea08ad7018ce31b77350aca8b07ca
walter espino <walterespino2025@outlook.com>
2025-03-13 17:10:09 -0600

    added git ignore

---
+++ data/ProductsDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace net8_test_walter_espino.data
+{
+    public class ProductsDBContext(DbContextOptions<ProductsDBContext> options) : DbContext(options) 
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- obj/Debug/net8.0-windows/net8_test_walter_espino.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/net8_test_walter_espino.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("net8_test_walter_espino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac580590b2e749684a702f2cc57c6d05f563d01f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e886d53f049498e889a98906fe14e72e4e27950")]
 [assembly: System.Reflection.AssemblyProductAttribute("net8_test_walter_espino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("net8_test_walter_espino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
