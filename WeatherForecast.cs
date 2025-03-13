cbb7e8c9304d9de34f0fcc1f918c5a41534dbbf6
walter espino <wevster@gmail.com>
2025-03-13 14:30:11 -0600

    init main

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace net8_test_walter_espino
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
